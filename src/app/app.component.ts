e336058fbb28d72af4520961bd2deb5e8074cf57
Juan C Pena <Ragevortex@users.noreply.github.com>
2021-03-01 19:49:16 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "Golem Works LLC";
+  copyright="(c) Golemworks LLC";
+}
